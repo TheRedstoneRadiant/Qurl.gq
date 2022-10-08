8cb4371b1dead352fc0d9f9ade40be19af8f42ac
Eesa Zahed <eesa.zahed@gmail.com>
2022-10-08 17:23:13 -0500

    fix: added req.socket.remoteAddress for IP logger

--- router/shortUrl.js
+++ router/shortUrl.js
@@ -4,44 +4,47 @@ const { urlCollection } = require("../");
 
 const router = Router();
 
-router.get('/:shortUrl*', async (req, res, next) => {
-    const shortenedUrl = await urlCollection.findOne({
-        shortUrl: req.params.shortUrl
-    });
+router.get("/:shortUrl*", async (req, res, next) => {
+  const shortenedUrl = await urlCollection.findOne({
+    shortUrl: req.params.shortUrl,
+  });
 
-    if (!shortenedUrl) {
-        return res.redirect("/404");
-    };
+  if (!shortenedUrl) {
+    return res.redirect("/404");
+  }
 
-    req.shortenedUrl = shortenedUrl;
+  req.shortenedUrl = shortenedUrl;
 
-    next();
+  next();
 });
 
-router.get('/:shortUrl', async (req, res, next) => {
-    res.redirect(req.shortenedUrl.destination);
+router.get("/:shortUrl", async (req, res, next) => {
+  res.redirect(req.shortenedUrl.destination);
 
-    const update = { $inc: { redirects: 1 } };
+  const update = { $inc: { redirects: 1 } };
 
-    // IP address logging
-    if (req.shortenedUrl.logIps) {
-        const ipAddress = req.header('x-forwarded-for');
-        
-        let location;
+  // IP address logging
+  if (req.shortenedUrl.logIps) {
+    const ipAddress =
+      req.headers["x-forwarded-for"] || req.socket.remoteAddress || "";
 
-        try {
-            const response = await axios.get(`https://ipapi.co/${ipAddress}/json/`);
-            location = `${response.data.city}, ${response.data.region}, ${response.data.country_name}`;
-        } catch {};
+    let location;
 
-        update["$push"] = { visitors: { ipAddress, location, time: Date.now() } };
+    try {
+      const response = await axios.get(`https://ipapi.co/${ipAddress}/json/`);
+      location = `${response.data.city}, ${response.data.region}, ${response.data.country_name}`;
+    } catch {
+      location = "";
     }
 
-    urlCollection.updateOne({ shortUrl: req.shortenedUrl.shortUrl }, update);
+    update["$push"] = { visitors: { ipAddress, location, time: Date.now() } };
+  }
+
+  urlCollection.updateOne({ shortUrl: req.shortenedUrl.shortUrl }, update);
 });
 
-router.get('/:shortUrl/info', async (req, res, next) => {
-    res.render("info", { ...req.shortenedUrl });
+router.get("/:shortUrl/info", async (req, res, next) => {
+  res.render("info", { ...req.shortenedUrl });
 });
 
 module.exports = router;
