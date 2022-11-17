b2660f1a0f0eebb77b90109d9cc267ceea361931
Your Name <agandhi29@myseneca.ca>
2022-11-17 13:16:44 -0500

    test1

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,24 +1,22 @@
 let map;
-const mapToken = process.env.MAPBOX_API_TOKEN;
-const mapboxgl = require('mapbox-gl');
 
-if (visitors.length !== 0) {// eslint-disable-line
-  mapboxgl.accessToken = mapToken;
+if (visitors.length !== 0) { // eslint-disable-line
+  mapboxgl.accessToken = mapToken; // eslint-disable-line
 
-  map = new mapboxgl.Map({
+  map = new mapboxgl.Map({// eslint-disable-line
     container: 'map',
     style: `mapbox://styles/mapbox/${dataTheme}-v10`, // eslint-disable-line
   });
 
-  const bounds = new mapboxgl.LngLatBounds(); // create map bounds
+  const bounds = new mapboxgl.LngLatBounds(); // eslint-disable-line
 
-  for (const visitor of JSON.parse(visitors)) { // eslint-disable-line
+  for (const visitor of JSON.parse(visitors)) {// eslint-disable-line
     // create an empty marker element
     const element = document.createElement('div');
     element.className = 'marker';
 
     // create and apply marker object
-    new mapboxgl.Marker({
+    new mapboxgl.Marker({// eslint-disable-line
       element,
     })
       .setLngLat(visitor.coordinates)
