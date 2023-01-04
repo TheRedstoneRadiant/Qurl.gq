d69a0dc6f4cc3b446ff60f620c01efff34781553
Eesa Zahed <eesa.zahed@gmail.com>
2023-01-03 22:24:34 -0500

    removed

--- router/rickroll.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-router.get('/rickroll', (req, res) => {
-  res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-});
-
-module.exports = router;
